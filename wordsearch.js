20aa43882c5813bbd53c497b188bb5ad36dbbc9a
whyttte <fogboruche@yahoo.com>
2020-08-17 09:41:44 +0000

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// pair programmed by @quinnvoker and @whyttte
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    if (letters.length < 1) {
+      return;
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    // vertical
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  
+    return false;
+};
+  
+const transpose = function(matrix) {
+    const transposedArr = [];
+    for (let x = 0; x < matrix[0].length; x++) {
+      transposedArr[x] = [];
+      for (let y = 0; y < matrix.length; y++) {
+        transposedArr[x][y] = matrix[y][x];
+      }
+    }
+    return transposedArr;
+};
+  
+module.exports = wordSearch;
